6ba70b905311421f629628dea8742570213f4102
MarioBanovac <marioban3@gmail.com>
2022-07-01 12:14:35 +0200

    added routes for the rest of the CRUD operations

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -8,3 +8,29 @@ router.get("/todos", async (req, res) => {
   const todos = await Todo.find();
   res.json(todos);
 });
+
+// route za nove todos
+router.post("/todos/newTodo", async (req, res) => {
+  const todo = new Todo({
+    title: req.body.title,
+  });
+  todo.save();
+  res.json(todo);
+});
+
+router.put("/todos/:id", async (req, res) => {
+  const todo = await Todo.findById(req.params.id);
+
+  todo.title = req.body.title;
+  todo.save();
+  res.json(todo);
+});
+
+// route za brisat todo pomocu id-a koji se generira u bazi autoomatski
+// findByIdAndDelete je metoda od moongose
+router.delete("/todos/:id", async (req, res) => {
+  const deletedTodo = await Todo.findByIdAndDelete(req.params.id);
+  res.json(deletedTodo);
+});
+
+module.exports = router;
